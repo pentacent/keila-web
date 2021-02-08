9d6c7a677cbda8086051837cd3e01f303f1fc6d9
Philipp <git@pentacent.com>
2021-02-08 12:25:45 +0100

    Add Matomo

--- nuxt.config.js
+++ nuxt.config.js
@@ -34,7 +34,16 @@ export default {
   ],
 
   // Modules: https://go.nuxtjs.dev/config-modules
-  modules: [],
+  modules: [
+    [
+      'nuxt-matomo',
+      {
+        matomoUrl: 'https://tracking.vanbittern.com/',
+        siteId: 33,
+        cookies: false,
+      },
+    ],
+  ],
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {},
