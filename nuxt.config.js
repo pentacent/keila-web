f63dd1665173889ac3969470413e5d5ada5c8601
Philipp <git@pentacent.com>
2021-02-08 01:11:11 +0100

    Configure site title

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,7 +4,7 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'keila-web',
+    title: 'Keila - Open Source Newsletters',
     htmlAttrs: {
       lang: 'en',
     },
